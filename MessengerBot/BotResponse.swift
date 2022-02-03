2e9a329f4d79d89a11fbcee01ad8087d9e3f3660
Justin Bui <justinqbui@gmail.com>
2022-02-03 11:52:57 -0800

    adding files

---
+++ MessengerBot/BotResponse.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+
+func getBotResponse(message: String) -> String {
+    let tempMessage = message.lowercased()
+    
+    if tempMessage.contains("hello") {
+        return "Hey there!"
+    } else if tempMessage.contains("goodbye") {
+        return "Talk to you later!"
+    } else if tempMessage.contains("how are you") {
+        return "I'm fine, how about you?"
+    } else if tempMessage.contains("AI") {
+        return "GPT-3 comming soon"
+    } else {
+        return "That's cool."
+    }
+}
---
+++ MessengerBot/MessengerBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerBotApp.swift
+//  MessengerBot
+//
+//  Created by Justin Bui on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MessengerBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
